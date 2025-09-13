23a980650a09396a101661f8f02328567bad8047
ms-akash <msakash2003@gmail.com>
2025-09-13 23:03:01 +0530

    Add mouseListener & mouseMotionListener in GamePanel

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,13 +1,21 @@
 package main;
 
 import inputs.KeyboardInputs;
+import inputs.MouseInputs;
 
 import javax.swing.JPanel;
 import java.awt.Graphics;
 
 public class GamePanel extends JPanel {
+
+    private MouseInputs mouseInputs;
+
     public GamePanel() {
+        mouseInputs = new MouseInputs();
+
         addKeyListener(new KeyboardInputs());
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
     }
 
     public void paintComponent(Graphics g) {
