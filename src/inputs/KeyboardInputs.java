fee0c7a8efc6582008b731aca05cf78bff734e18
ms-akash <msakash2003@gmail.com>
2025-09-13 23:48:51 +0530

    Add keyboard movements to the component in GamePanel

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -1,9 +1,18 @@
 package inputs;
 
+import main.GamePanel;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyboardInputs implements KeyListener {
+
+    private GamePanel gamePanel;
+
+    public KeyboardInputs(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+    }
+
     @Override
     public void keyTyped(KeyEvent e) {
 
@@ -13,16 +22,16 @@ public class KeyboardInputs implements KeyListener {
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_W:
-                System.out.println("W");
+                this.gamePanel.changeYDelta(-5);
                 break;
             case KeyEvent.VK_A:
-                System.out.println("A");
+                this.gamePanel.changeXDelta(-5);
                 break;
             case KeyEvent.VK_S:
-                System.out.println("S");
+                this.gamePanel.changeYDelta(5);
                 break;
             case KeyEvent.VK_D:
-                System.out.println("D");
+                this.gamePanel.changeXDelta(5);
                 break;
         }
     }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -9,18 +9,29 @@ import java.awt.Graphics;
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
+    private int xDelta = 0, yDelta = 0;
 
     public GamePanel() {
         mouseInputs = new MouseInputs();
 
-        addKeyListener(new KeyboardInputs());
+        addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
+    }
+
+    public void changeXDelta(int xDelta) {
+        this.xDelta += xDelta;
+        repaint();
+    }
+
+    public void changeYDelta(int yDelta) {
+        this.yDelta += yDelta;
+        repaint();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.fillRect(100, 100, 500, 200);
+        g.fillRect(100 + xDelta, 100 + yDelta, 500, 200);
     }
 }
